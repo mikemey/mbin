e4db5f0e45d14b2d24e09419f4f6fc6d6065c2e5
mikemey <miguel.maestro@gmx.co.uk>
2019-03-09 22:52:42 +0000

    refactor parameter-format from ut to haumea script, due to inconsistent parameter pass behaviour of ut

--- mercury/transfer_haumea.sh
+++ mercury/transfer_haumea.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-new_ft=`cygpath -u $1`
-if [[ -e $new_ft ]]; then
-  haumea "$new_ft"
-else
-  echo "Not found: $new_ft"
+IFS='|'; params=($@); unset IFS;
+mode="${params[0]}"
+dir="${params[1]}"
+file="${params[2]}"
+label="${params[3]}"
+
+[[ "$label" != "" ]] && exit 0
+
+function not_found () {
+  echo "Not found: $1"
   read
   exit 1
+}
+
+haumea_arg=`cygpath -u "$dir"`
+if [[ "$mode" != "multi" ]]; then
+  haumea_arg="${haumea_arg}${file}"
 fi
 
+[[ -e "$haumea_arg" ]] || not_found "$haumea_arg"
+haumea "$haumea_arg"
 exit 0
