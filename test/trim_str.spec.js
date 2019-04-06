c74ce7ebd21734b25d00441f761f0ca2c0758ede
mikemey <miguel.maestro@gmx.co.uk>
2019-04-06 21:39:23 +0100

    implement full trimming of whitespaces + add tests

--- test/trim_str.spec.js
+++ test/trim_str.spec.js
@@ -4,11 +4,13 @@ const { ScriptRunner } = require('./scriptRunner')
 
 describe('trim_str tests', () => {
   const runner = () => ScriptRunner()
-  const verboseRunner = () => ScriptRunner({ verbose: true })
 
-  const escape = v => v.replace(/\n/g, '\\n').replace(/\t/g, '\\t')
+  const escape = v => v.replace(/\n/g, '\\n').replace(/\t/g, '\\t').replace(/\r/g, '\\r')
 
   const inputStr = new Map()
+    .set('abc', 'abc')
+    .set('abc def', 'abc def')
+    .set('abc\t\ndef', 'abc\t\ndef')
     .set('', '')
     .set('   ', '')
     .set(' \n  \t  ', '')
@@ -20,20 +22,20 @@ describe('trim_str tests', () => {
 
     .set('\nabc05', 'abc05')
     .set('abc06\n', 'abc06')
-  //  .set('\nabc07\n', 'abc07')
-  //  .set('\nabc08\ndef\n', 'abc08\ndef')
+    .set('\nabc07\n', 'abc07')
+    .set('\nabc08\ndef\n', 'abc08\ndef')
 
-  //     .set('\tabc09', 'abc09')
-  //     .set('abc10\t', 'abc10')
-  //     .set('\tabc11\t', 'abc11')
-  // .set('\tabc12\tdef', 'abc12\tdef')
+    .set('\tabc09', 'abc09')
+    .set('abc10\t', 'abc10')
+    .set('\tabc11\n\t', 'abc11')
+    .set('\n \t  \n   abc12 \n   \t def \n   \t  ', 'abc12 \n   \t def')
 
-  // .set('\r\n  abc13 \t def  \t  ', 'abc13 \t def')
-  // .set(' \r\nabc14 \n def  \n ', 'abc14')
-  // .set('\t  abc15 \t\n def \t \r\n ', 'abc15 \t\n def')
+    .set('\r\n  abc13 \t def  \t  ', 'abc13 \t def')
+    .set(' \r\nabc14 \n def  \n ', 'abc14 \n def')
+    .set('\t  abc15 \t\n def \t \r\n ', 'abc15 \t\n def')
 
   inputStr.forEach((value, key) => {
-    it.only(`trims string '${escape(key)}' --> '${escape(value)}'`, () => verboseRunner()
+    it(`trims string '${escape(key)}' --> '${escape(value)}'`, () => runner()
       .command('trim_str', key)
       .expectOutput(value)
       .execute()
