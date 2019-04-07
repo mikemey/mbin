330013c96496b323332dd93bfa79d41f928b273e
mikemey <miguel.maestro@gmx.co.uk>
2019-04-07 11:26:48 +0100

    implement tests for parameter input checks

--- test/common/open_connections.spec.js
+++ test/common/open_connections.spec.js
@@ -3,11 +3,15 @@ require('chai').should()
 const { Bocks } = require('../../bocks')
 
 describe('open_connections tests', () => {
-  xit('runs open_connections', () => Bocks({ verbose: true })
-    .command('open_connections')
-    .expectOutput(out => {
-      out.should.match(/Missing program parametexr/)
-    })
-    .execute()
-  )
+  const Verbose = () => Bocks({ verbose: true })
+
+  describe.only('input parameter checks', () => {
+    const expectMissingProgramParameter = (...commands) => Bocks()
+      .command(...commands)
+      .expectOutput(/Missing program parameter/)
+      .execute()
+
+    it('no parameters', () => expectMissingProgramParameter('open_connections'))
+    it('single use no parameters', () => expectMissingProgramParameter('open_connections', '-s'))
+  })
 })
