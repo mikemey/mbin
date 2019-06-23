f8d3f4473e49cb338320951670f79690570daa2f
mikemey <miguel.maestro@gmx.co.uk>
2019-06-23 19:29:47 +0100

    implement msm report generator

---
+++ memoria/msm_report.py
@@ -0,0 +1,46 @@
+#!/usr/local/bin/python
+import os
+import requests
+import mail_sender as mails
+
+wr_murl = 'https://{}/workout-records/api/metadata'.format(os.environ['MSMSERVER'])
+tantalus_murl = 'https://{}/api/metadata/schedule'.format(os.environ['MSMSERVER'])
+
+report_template = '''
+<b>MSM Services report</b>
+
+Tantalus-scheduler:
+Latest run: {}
+Latest captured (ticker/graphs): {} / {}
+
+Workout Records:
+Request log file size: {} kb
+'''
+
+
+def notify(tantalus_meta, wr_meta):
+    print('sending report...')
+    wr_req_size = wr_meta['requestLogSize'] // 1024
+    report = report_template.format(
+        tantalus_meta['created'], tantalus_meta['ticker']['count'], tantalus_meta['graphs']['count'],
+        wr_req_size
+    )
+    print (report)
+    mails.send('[MSM report] service report', report)
+
+
+def get_metadata(url):
+    resp = requests.get(url)
+    resp.raise_for_status()
+    return resp.json()
+
+
+try:
+    print('checking...')
+    wr_metadata = get_metadata(wr_murl)
+    tantalus_metadata = get_metadata(tantalus_murl)
+    notify(tantalus_metadata, wr_metadata)
+    print('done')
+except Exception as ex:
+    print('error: {}'.format(ex))
+    mails.send('[MSM report] Server check error', 'An error occurred:\n{}'.format(ex))
