3a23cd1563dcff6929f820676bb53e5c03729bd0
mikemey <miguel.maestro@gmx.co.uk>
2019-11-02 18:24:49 +0000

    add gma-check to main-script

--- memoria/checks/check_main.sh
+++ memoria/checks/check_main.sh
@@ -2,3 +2,4 @@
 
 python3 $MBIN/checks/vfuet_check.py | timelog >> $LOGDIR/vfuet/check.log 2>&1
 python3 $MBIN/checks/monero_check.py | timelog >> $LOGDIR/monero/check.log 2>&1
+python3 $MBIN/checks/gma_version_check.py | timelog >> $LOGDIR/versions/gma_check.log 2>&1
