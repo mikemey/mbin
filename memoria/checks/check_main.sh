9ee5c87ba35fbe1dd3c349799eeff8f74ea7ff9d
mikemey <miguel.maestro@gmx.co.uk>
2019-11-02 18:51:09 +0000

    add gpc check script

--- memoria/checks/check_main.sh
+++ memoria/checks/check_main.sh
@@ -3,3 +3,4 @@
 python3 $MBIN/checks/vfuet_check.py | timelog >> $LOGDIR/vfuet/check.log 2>&1
 python3 $MBIN/checks/monero_check.py | timelog >> $LOGDIR/versions/monero_check.log 2>&1
 python3 $MBIN/checks/gma_version_check.py | timelog >> $LOGDIR/versions/gma_check.log 2>&1
+python3 $MBIN/checks/gpc_version_check.py | timelog >> $LOGDIR/versions/gpc_check.log 2>&1
---
+++ memoria/checks/gpc_version_check.py
@@ -0,0 +1,61 @@
+#!/usr/local/bin/python
+
+import os
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+sys.path.append(os.environ['MBIN'])
+import mail_sender as mails
+
+url = 'https://github.com/googleads/googleads-consent-sdk-ios/releases'
+captured_fname = '{}/versions/gpc.txt'.format(os.environ['LOGDIR'])
+
+
+def request_current_version():
+    resp = requests.get(url)
+    resp.raise_for_status()
+    html = BeautifulSoup(resp.text, 'html.parser')
+    version_divs = html.select('div.f1')
+    if not version_divs:
+        return None
+    return version_divs[0].text.strip()
+
+
+def read_captured_versions():
+    file_mode = 'r' if os.path.exists(captured_fname) else 'a+'
+    with open(captured_fname, file_mode) as fin:
+        return [line.strip() for line in fin.readlines()]
+
+
+def add_captured_version(new_version):
+    with open(captured_fname, 'a') as f:
+        f.write('{}\n'.format(new_version))
+
+
+def notify(msg):
+    if isinstance(msg, Exception):
+        print('error: {}'.format(msg))
+        mails.send('[GPC] check error', 'An error occurred:\n{}'.format(msg))
+    elif msg is False:
+        print('no results')
+        mails.send('[GPC] no results', 'notext')
+    else:
+        print('new version: {}'.format(msg))
+        mails.send('[NEW GPC] {}'.format(msg), 'notext')
+
+
+try:
+    print('checking...')
+    current_version = request_current_version()
+    captured_versions = read_captured_versions()
+
+    if current_version is None:
+        notify(False)
+    elif current_version not in captured_versions:
+        notify(current_version)
+        add_captured_version(current_version)
+    print('done')
+except Exception as ex:
+    notify(ex)
