24865373e0c84dbac73508ac3639252d71b3656f
mikemey <miguel.maestro@gmx.co.uk>
2019-11-06 15:00:01 +0000

    add electrum version check

---
+++ memoria/checks/electrum_version_check.py
@@ -0,0 +1,54 @@
+#!/usr/local/bin/python
+
+import os
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+from check_file import CheckFile
+
+sys.path.append(os.environ['MBIN'])
+import mail_sender as mails
+
+url = 'https://electrum.org/panel-download.html'
+captured_fname = '{}/versions/electrum.txt'.format(os.environ['LOGDIR'])
+find_text = "Latest release: "
+
+
+def request_current_version():
+    resp = requests.get(url)
+    resp.raise_for_status()
+    html = BeautifulSoup(resp.text, 'html.parser')
+    version_header = html.find('h2')
+    if not version_header or not version_header.text.startswith(find_text):
+        return None
+    return version_header.text[len(find_text):]
+
+
+def notify(msg):
+    if isinstance(msg, Exception):
+        print('error: {}'.format(msg))
+        mails.send('[electrum] check error', 'An error occurred:\n{}'.format(msg))
+    elif msg is False:
+        print('no results')
+        mails.send('[electrum] no results', 'notext')
+    else:
+        print('new version: {}'.format(msg))
+        mails.send('[NEW electrum] {}'.format(msg), 'notext')
+
+
+try:
+    out_file = CheckFile(captured_fname)
+    print('checking...')
+    current_version = request_current_version()
+    captured_versions = out_file.read_entries()
+
+    if current_version is None:
+        notify(False)
+    elif current_version not in captured_versions:
+        notify(current_version)
+        out_file.write_entry(current_version)
+    print('done')
+except Exception as ex:
+    notify(ex)
