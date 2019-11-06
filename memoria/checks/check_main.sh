a3046a711b4ff285bed0c785bb1719f72456174a
mikemey <miguel.maestro@gmx.co.uk>
2019-11-06 15:30:25 +0000

    add start/end log

--- memoria/checks/check_main.sh
+++ memoria/checks/check_main.sh
@@ -7,9 +7,11 @@ function run_check () {
   echo "[${py_script}]: $?"
 }
 
+echo "starting checks..."
 run_check "vfuet_check.py" "$LOGDIR/vfuet/check.log"
 run_check "monero_check.py" "$LOGDIR/versions/monero_check.log "
 run_check "gma_version_check.py" "$LOGDIR/versions/gma_check.log"
 run_check "gpc_version_check.py" "$LOGDIR/versions/gpc_check.log "
 run_check "electrum_version_check.py" "$LOGDIR/versions/electrum_check.log"
 run_check "bitcoind_version_check.py" "$LOGDIR/versions/bitcoind_check.log "
+echo "done"
