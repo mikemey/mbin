b3868239af0744a5412481266e9260b0c3be96e5
mikemey <miguel.maestro@gmx.co.uk>
2019-11-06 19:21:30 +0000

    fix log-file name + location

--- memoria/checks/check_main.sh
+++ memoria/checks/check_main.sh
@@ -6,15 +6,15 @@ export CHECK_LOG_DIR="$LOGDIR/checks"
 function run_check () {
   py_script="$1"
   log_file="$2"
-  python3 $PY_CHECKS/$py_script | timelog >> "$CHECK_LOG_DIR/$log_file" 2>&1
+  python3 $PY_CHECKS/$py_script | timelog >> "${CHECK_LOG_DIR}/${log_file}" 2>&1
   echo "[${py_script}]: $?"
 }
 
 echo "starting checks..."
 run_check "vfuet_check.py" "vfuet.log"
-run_check "monero_check.py" "monero_check.log "
+run_check "monero_check.py" "monero_check.log"
 run_check "gma_version_check.py" "gma_check.log"
-run_check "gpc_version_check.py" "gpc_check.log "
+run_check "gpc_version_check.py" "gpc_check.log"
 run_check "electrum_version_check.py" "electrum_check.log"
-run_check "bitcoind_version_check.py" "bitcoind_check.log "
+run_check "bitcoind_version_check.py" "bitcoind_check.log"
 echo "done"
