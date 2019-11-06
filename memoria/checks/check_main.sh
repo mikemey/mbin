60fa2438feb39af33d7195718d29a23b6e4c163e
mikemey <miguel.maestro@gmx.co.uk>
2019-11-06 15:27:06 +0000

    add log of script exit-code

--- memoria/checks/check_main.sh
+++ memoria/checks/check_main.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-python3 $MBIN/checks/vfuet_check.py | timelog >> $LOGDIR/vfuet/check.log 2>&1
-python3 $MBIN/checks/monero_check.py | timelog >> $LOGDIR/versions/monero_check.log 2>&1
-python3 $MBIN/checks/gma_version_check.py | timelog >> $LOGDIR/versions/gma_check.log 2>&1
-python3 $MBIN/checks/gpc_version_check.py | timelog >> $LOGDIR/versions/gpc_check.log 2>&1
-python3 $MBIN/checks/electrum_version_check.py | timelog >> $LOGDIR/versions/electrum_check.log 2>&1
-python3 $MBIN/checks/bitcoind_version_check.py | timelog >> $LOGDIR/versions/bitcoind_check.log 2>&1
+function run_check () {
+  py_script="$1"
+  log_file="$2"
+  python3 $MBIN/checks/$py_script | timelog >> "$log_file" 2>&1
+  echo "[${py_script}]: $?"
+}
+
+run_check "vfuet_check.py" "$LOGDIR/vfuet/check.log"
+run_check "monero_check.py" "$LOGDIR/versions/monero_check.log "
+run_check "gma_version_check.py" "$LOGDIR/versions/gma_check.log"
+run_check "gpc_version_check.py" "$LOGDIR/versions/gpc_check.log "
+run_check "electrum_version_check.py" "$LOGDIR/versions/electrum_check.log"
+run_check "bitcoind_version_check.py" "$LOGDIR/versions/bitcoind_check.log "
