7faf7a023b3c3be405b6e8395f8de17f8cf84990
mikemey <miguel.maestro@gmx.co.uk>
2019-11-07 15:29:49 +0000

    refactor to parse html

--- memoria/checks/monero_check.py
+++ memoria/checks/monero_check.py
@@ -5,38 +5,37 @@ import sys
 import traceback
 
 import requests
+from bs4 import BeautifulSoup
 
 sys.path.append(os.environ['MBIN'])
 import mail_sender as mails
 from check_file import CheckFile
 
-url = 'https://downloads.getmonero.org/cli/win64'
+url = 'https://web.getmonero.org/downloads/'
 captured_fname = sys.argv[1]
-max_redirects = 5
+find_text = "Current Version: "
 
 
 def request_version():
-    redirects = max_redirects
-    location = location_of(url)
-    while redirects > 0 and not location.endswith('zip'):
-        redirects -= 1
-        location = location_of(location)
-    if location:
-        return location.rsplit('/', 1)[-1]
-    else:
-        raise Exception('no download file found!')
-
-
-def location_of(request_url):
-    resp = requests.options(request_url, allow_redirects=False, timeout=10)
+    resp = requests.get(url, timeout=10)
     resp.raise_for_status()
-    return resp.headers.get('location')
+    html = BeautifulSoup(resp.text, 'html.parser')
+    cli_anchor = html.find(href='//downloads.getmonero.org/cli/win64')
+    if not cli_anchor:
+        return None
+    version_p = cli_anchor.parent.find_next_sibling('p')
+    if not version_p or not version_p.text.startswith(find_text):
+        return None
+    return version_p.text[len(find_text):]
 
 
 def notify(msg):
     if isinstance(msg, Exception):
         print('error: {}'.format(msg))
         mails.send('[MONERO] check error', 'An error occurred:\n{}'.format(msg))
+    elif msg is False:
+        print('no results')
+        mails.send('[MONERO] no results', 'notext')
     else:
         print('new version: {}'.format(msg))
         mails.send('[MONERO] {}'.format(msg), 'notext')
@@ -46,12 +45,15 @@ exit_code = 0
 try:
     out_file = CheckFile(captured_fname)
     print('checking...')
-    available_version = request_version()
+    current_version = request_version()
     captured_versions = out_file.read_entries()
 
-    if available_version not in captured_versions:
-        notify(available_version)
-        out_file.write_entry(available_version)
+    if current_version is None:
+        notify(False)
+        exit_code = 1
+    if current_version not in captured_versions:
+        notify(current_version)
+        out_file.write_entry(current_version)
     print('done')
 except Exception as ex:
     traceback.print_exc(file=sys.stderr)
