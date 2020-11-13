a616572074f16e6c0b59ff7f64ea09267b065337
mikemey <miguel.maestro@gmx.co.uk>
2020-11-13 20:52:30 +0100

    swap memoria/haumea transfer order

--- hyperion/transfer_data.sh
+++ hyperion/transfer_data.sh
@@ -64,6 +64,6 @@ esac
 [[ -e "$file" ]] || error_message "File/Directory not found: $file"
 
 ($SEND_TO_HAUMEA || $SEND_TO_MEMORIA) && timelog "${FG_GREEN}start transfer [$file]${FG_DEFAULT}"
-$SEND_TO_HAUMEA  && send_command "haumea" "$file" "$HAUMEA"
 $SEND_TO_MEMORIA && send_command "memoria -c" "$file" "$MEMORIA"
+$SEND_TO_HAUMEA  && send_command "haumea" "$file" "$HAUMEA"
 ($SEND_TO_HAUMEA || $SEND_TO_MEMORIA) && timelog "${FG_GREEN}transfer complete.${FG_DEFAULT}"
