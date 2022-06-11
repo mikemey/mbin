9f38528ee3c26b9b1ff2284d09597d388d8b911a
mikemey <miguel.maestro@gmx.co.uk>
2022-06-11 13:27:02 +0200

    add fronius check

---
+++ memoria/checks/fronius_check.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python
+
+from bs4 import BeautifulSoup
+
+from check_utils import run_generic_check
+
+url = 'https://www.fronius.com/en/solar-energy/installers-partners/service-support/tech-support/software-and-updates/symo-gen24plus-update'
+find_text = "Latest release: "
+
+
+def extract_version_from(html: BeautifulSoup):
+    version = html.find_all('span', class_='title')[2]
+    return version.text
+
+
+run_generic_check('Fronius', url, extract_version_from)
