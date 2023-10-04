8d6bad5afdd91d4fa8da19683a7af01c5e8dd2ae
mikemey <miguel.maestro@gmx.co.uk>
2023-10-04 19:37:05 +0200

    remove whitespace replacement

--- common/ep_finder.py
+++ common/ep_finder.py
@@ -41,7 +41,6 @@ def query_episodes(id):
             print('------ Season', ep.season, '---------')
             curr_season = ep.season
         ep_name = BeautifulSoup(ep.name, 'html.parser').text
-        ep_name = ep_name.replace(' ', '.')
         print(f'S{ep.season:02}E{ep.episode:02}: "{ep_name}"')
 
 # query_episode_id('season_name')
