228766b40789a8138b284b68306a12ce6443a074
mikemey <miguel.maestro@gmx.co.uk>
2024-02-03 08:52:19 +0100

    add details to tickets, enable meil send

--- memoria/checks/tickets_check.py
+++ memoria/checks/tickets_check.py
@@ -15,17 +15,27 @@ def request_tickets():
 
 
 def extract_tickets_from(html: BeautifulSoup):
-    ticket_data = html.select('.SmallerGrad2')
-    return [data.text for data in ticket_data]
+    ticket_data = html.select('.gamehome')
+    return [TicketAnnouncement(data) for data in ticket_data]
+
+
+class TicketAnnouncement:
+    def __init__(self, ticket_html):
+        self.date = ticket_html.select_one('.date').text
+        headline = ticket_html.select_one('span[class^=Smaller]').text
+        self.headline = headline.replace('Puntigamer ', '')
+
+    def getId(self):
+        return self.date + self.headline
 
 
 def __send_mail(title, body):
-    send_mail("Tickets", url, title, body, True)
+    send_mail("Tickets", url, title, body)
 
 
 def create_body_from(tickets):
-    cleanup = map(lambda t: '- ' + t.replace('SK Puntigamer ', ''), tickets)
-    return "\n".join(cleanup)
+    lines = map(lambda t: f'- {t.date}\n\t{t.headline}', tickets)
+    return "\n".join(lines)
 
 
 def run_check():
@@ -41,10 +51,10 @@ def run_check():
         else:
             out_file = CheckFile(check_file_name)
             stored_tickets = out_file.read_entries()
-            new_tickets = [t for t in tickets if t not in stored_tickets]
+            new_tickets = [t for t in tickets if t.getId() not in stored_tickets]
             if len(new_tickets) > 0:
                 __send_mail('New tickets', create_body_from(new_tickets))
-                out_file.write_entries(new_tickets)
+                out_file.write_entries([t.getId() for t in new_tickets])
         print('done')
     except Exception as ex:
         traceback.print_exc(file=sys.stderr)
