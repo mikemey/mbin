d16739461cb823a4b268eb5219245821864c9b60
Michael Mayer <michael.mayer@bearingpoint.com>
2025-06-29 19:15:22 +0200

    update fronius check script (lazy-loaded anchors)

--- memoria/checks/fronius_check.py
+++ memoria/checks/fronius_check.py
@@ -1,16 +1,21 @@
 #!/usr/local/bin/python
+import re
 
 from bs4 import BeautifulSoup
 
 from check_utils import run_generic_check
 
 url = 'https://www.fronius.com/en/solar-energy/installers-partners/service-support/tech-support/software-and-updates/symo-gen24plus-update'
-find_text = "Latest release: "
+
+SCRIPT_TEXT_FIND_PATTERN = r'"text":\s*"(?P<value>Firmware Fronius Update[^"]*)'
 
 
 def extract_version_from(html: BeautifulSoup):
-    version = html.find_all('span', class_='title')[3]
-    return version.text
+    script = html.find('script', {"id": "__NEXT_DATA__"})
+    match = re.search(SCRIPT_TEXT_FIND_PATTERN, script.text)
+    if match:
+        return match.group('value')
+    raise Exception("Version not found in the HTML content.")
 
 
 run_generic_check('Fronius', url, extract_version_from)
